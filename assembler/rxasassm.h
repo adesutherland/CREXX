16297b479fcb4dafbd9008e818782866f83c4f41
Adrian Sutherland <adrian@sutherlandonline.org>
2021-06-06 21:08:40 +0100

    i0052

--- assembler/rxasassm.h
+++ assembler/rxasassm.h
@@ -4,7 +4,7 @@
 #ifndef CREXX_RXASASSM_H
 #define CREXX_RXASASSM_H
 
-#define rxversion "cREXX-Phase-0 v0.1.6-i0041"
+#define rxversion "cREXX-Phase-0 v0.1.6-i0052"
 
 #include "rxas.h"
 #include "rxasgrmr.h"
