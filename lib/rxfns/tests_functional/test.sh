9c503b777fad3e6ab1c9559f6b1efc60cdfce1c8
René Jansen <rvjansen@xs4all.nl>
2021-10-06 23:44:21 +0200

    fix linkage conventions in right.rxas + added tests

--- lib/rxfns/tests_functional/test.sh
+++ lib/rxfns/tests_functional/test.sh
@@ -1,4 +1,5 @@
 rxvm tsubstr ../rexx/substr ../rexx/length ../rexx/raise
 rxvm tsubstr ../rxas/substr ../rxas/length
-rxvm tsubstr ../rxas/oldsubstr ../rxas/length
+rxvm ts_right ../rxas/right ../rxas/copies ../rxas/length ../rxas/substr
+
 
