94ce351fb87b4b13e6960995aa5136bef8d1aac3
René Jansen <rvjansen@xs4all.nl>
2021-10-07 13:27:16 +0200

    length testcase

--- lib/rxfns/tests_functional/test.sh
+++ lib/rxfns/tests_functional/test.sh
@@ -2,4 +2,5 @@ rxvm tsubstr ../rexx/substr ../rexx/length ../rexx/raise
 rxvm tsubstr ../rxas/substr ../rxas/length
 rxvm ts_right ../rxas/right ../rxas/copies ../rxas/length ../rxas/substr
 rxvm ts_left ../rxas/substr ../rxas/left ../rxas/length
-
+rxvm ts_length ../rexx/length
+rxvm ts_length ../rxas/length
