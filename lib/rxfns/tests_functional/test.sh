97df54452f093689b5a8eb2a780e52a76412d9fb
René Jansen <rvjansen@xs4all.nl>
2021-10-08 14:12:37 +0200

    copies testcase

--- lib/rxfns/tests_functional/test.sh
+++ lib/rxfns/tests_functional/test.sh
@@ -1,6 +1,8 @@
-rxvm tsubstr ../rexx/substr ../rexx/length ../rexx/raise
+#rxvm tsubstr ../rexx/substr ../rexx/length ../rexx/raise
 rxvm tsubstr ../rxas/substr ../rxas/length
 rxvm ts_right ../rxas/right ../rxas/copies ../rxas/length ../rxas/substr
 rxvm ts_left ../rxas/substr ../rxas/left ../rxas/length
 rxvm ts_length ../rexx/length
 rxvm ts_length ../rxas/length
+rxvm ts_pos ../rxas/pos
+rxvm tscopies ../rxas/copies
