80601171985bebbd28d73e93c3a3f50e927e7c96
René Jansen <rvjansen@xs4all.nl>
2023-01-18 23:33:59 -0400

    corr sql

--- docs/instructions/instructiontable.sql
+++ docs/instructions/instructiontable.sql
@@ -1,18 +1,8 @@
-drop table instructions;
-CREATE TABLE location
-( instruction        integer primary key,
-  opcode             char(6),
-  mnemonic           varchar(50),
-  operands	     varchar(50),
-  description	     varchar(80),
-  text		     varchar(1000),
-  category	     integer
-);
-drop table category;
+drop table if exists category;
 CREATE TABLE category
 ( category           integer primary key,
   name               varchar(80),
-  description	     varchar(80),
+  description	     varchar(80)
 );
 insert into category VALUES(1,'Fixed Point Arithmetic','Instructions that work on integer numeric types');
 insert into category VALUES(2,'Floating Point Arithmetic','Instructions that work on floating point numeric types');
@@ -22,4 +12,15 @@ insert into category VALUES(5,'I/O operations','Instructions that implement inpu
 insert into category VALUES(6,'Time Instructions','Instructions that work with date and time');
 insert into category VALUES(7,'Meta Instructions','Instructions that work on program structure metadata');
 insert into category VALUES(8,'Breakpoint Instructions','Instructions that aid in debugging programs');
-COMMIT;
+drop table if exists instruction;
+CREATE TABLE instruction
+( instruction        integer primary key,
+  opcode             char(6),
+  mnemonic           varchar(50),
+  operands	     varchar(50),
+  description	     varchar(80),
+  text		     varchar(1000),
+  category	     integer,
+  foreign key (category) references category(category)
+);
+
