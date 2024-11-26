5c1a78511fb8558320853ddf146a59b30e6a83a7
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2024-11-26 10:25:57 +0100

    1. completed the RXMATH library 2. added a STACK plugin

--- lib/plugins/rxmath/rxmath.c
+++ lib/plugins/rxmath/rxmath.c
@@ -25,28 +25,91 @@
     MATHPROC(cos )
     MATHPROC(cosh)
     MATHPROC(exp )
+    MATHPROC(exp2)
     MATHPROC(log )
+    MATHPROC(log2)
     MATHPROC(log10)
-// MATHPROC(pow10)
+    MATHPROC(ceil)
+    MATHPROC(floor)
+    MATHPROC(fabs)
+    MATHPROC(round)
+    MATHPROC(trunc)
+
+    PROCEDURE(xfmod) {  //  ADDMATH uses functionX to distinguish plugin function name from math function name
+    RETURNFLOAT( fmod(GETFLOAT(ARG0), GETFLOAT(ARG1)));
+    PROCRETURN
+    ENDPROC}
+
     MATHPROC(sin )
     MATHPROC(sinh)
     MATHPROC(sqrt)
     MATHPROC(tan )
     MATHPROC(tanh)
+    MATHPROC(erf)
+    MATHPROC(erfc)
+    MATHPROC(tgamma)
+    MATHPROC(lgamma)
+    MATHPROC(asinh)
+    MATHPROC(acosh)
+    MATHPROC(atanh)
+
+PROCEDURE(xpow) {  //  ADDMATH uses functionX to distinguish plugin function name from math function name
+    RETURNFLOAT( pow(GETFLOAT(ARG0), GETFLOAT(ARG1)));
+    PROCRETURN
+ENDPROC}
+PROCEDURE(xpow10) {  //  ADDMATH uses functionX to distinguish plugin function name from math function name
+    RETURNFLOAT( pow(10.0, (GETFLOAT(ARG0))));
+    PROCRETURN
+    ENDPROC}
+
+PROCEDURE(xhypot) {  //  ADDMATH uses functionX to distinguish plugin function name from math function name
+    RETURNFLOAT(hypot(GETFLOAT(ARG0),GETFLOAT(ARG0)));
+    PROCRETURN
+    ENDPROC}
+
+PROCEDURE(pi) {
+    RETURNFLOAT(M_PI);
+    PROCRETURN
+ENDPROC
+}
+PROCEDURE(euler) {
+    RETURNFLOAT(M_E);
+    PROCRETURN
+    ENDPROC
+}
 // RXMATH function definitions
 LOADFUNCS
     ADDMATH(acos)
     ADDMATH(asin)
     ADDMATH(atan)
-    ADDMATH(cos )
+    ADDMATH(cos)
     ADDMATH(cosh)
-    ADDMATH(exp )
-    ADDMATH(log )
+    ADDMATH(exp)
+    ADDMATH(exp2)
+    ADDMATH(log)
+    ADDMATH(log2)
     ADDMATH(log10)
-  //  ADDMATH(pow10)
-    ADDMATH(sin )
+    ADDPROC(xpow,"rxmath.pow",   "b",  ".float", "arg1=.float,arg2=.float");
+    ADDMATH(pow10)
+    ADDMATH(ceil)
+    ADDMATH(floor)
+    ADDMATH(fabs)
+    ADDMATH(round)
+    ADDMATH(trunc)
+    ADDPROC(xfmod,"rxmath.fmod",   "b",  ".float", "arg1=.float,arg2=.float");
+    ADDMATH(sin)
     ADDMATH(sinh)
     ADDMATH(sqrt)
-    ADDMATH(tan )
+    ADDMATH(tan)
     ADDMATH(tanh)
-ENDLOADFUNCS
+    ADDMATH(erf)
+    ADDMATH(erfc)
+    ADDMATH(tgamma)
+    ADDMATH(lgamma)
+    ADDMATH(asinh)
+    ADDMATH(acosh)
+    ADDMATH(atanh)
+    ADDPROC(xhypot,"rxmath.hypot",   "b",  ".float", "arg1=.float,arg2=.float");
+    ADDPROC(pi,   "rxmath.pi",   "b",  ".float", "");
+    ADDPROC(euler,"rxmath.euler","b",  ".float", "");
+ ENDLOADFUNCS
