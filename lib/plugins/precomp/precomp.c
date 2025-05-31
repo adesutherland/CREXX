f8893914b5f73d0759a09f347fdbc61fa07ef39f
René Jansen <rvjansen@xs4all.nl>
2025-05-31 14:13:53 +0200

    fix for strupr() on mac

--- lib/plugins/precomp/precomp.c
+++ lib/plugins/precomp/precomp.c
@@ -463,7 +463,7 @@ PROCEDURE (fsearch) {
     char * ustr3= GETSTRING(ARG4);
     char * haystack;
     for (i = from; i < mmax; i++) {
-        haystack = strdup(strupr(GETSARRAY(ARG0, i)));
+        haystack = strdup(strupr_portable(GETSARRAY(ARG0, i)));
         if (ustr1[0] != '\0')
             if (strstr(haystack, GETSTRING(ARG2)) > 0) {
                 SETINT(ARG5, 1);
