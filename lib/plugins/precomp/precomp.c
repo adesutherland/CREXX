915c3299bb0c95e80f4ba65608c4a5ec5d6e8f4f
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2025-08-04 19:48:46 +0200

    precomp.c an Ubuntu/Windows incompability for Ubuntu corrected

--- lib/plugins/precomp/precomp.c
+++ lib/plugins/precomp/precomp.c
@@ -244,21 +244,6 @@ PROCEDURE(writeall) {
     PROCRETURN
     ENDPROC
 }
-static inline char *strcasestr(const char *haystack, const char *needle) {
-    if (!*needle) return (char *)haystack;
-
-    for (; *haystack; ++haystack) {
-        const char *h = haystack;
-        const char *n = needle;
-        while (*h && *n && tolower((unsigned char)*h) == tolower((unsigned char)*n)) {
-            ++h;
-            ++n;
-        }
-        if (!*n)
-            return (char *)haystack;
-    }
-    return NULL;
-}
 /* -------------------------------------------------------------------------------------
 * Portable strupr() because that is windows-only
 * -------------------------------------------------------------------------------------
